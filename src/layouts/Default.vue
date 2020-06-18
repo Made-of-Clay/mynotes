bb08d0e05cc36642889a35d2be2a228fc46edd4d
Adam <adam.m.leis@gmail.com>
2020-06-17 23:05:54 -0400

    remove footer
    also misc formatting

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,22 +3,16 @@
     <a class="visually-hidden" href="#main">Skip to content</a>
 
     <div id="wrapper" class="wrapper pb-16 md:pb-0 flex flex-col relative min-h-screen">
-
-      <HeaderPartial/>
-
+      <HeaderPartial />
 
       <main id="main" class="main inner flex flex-1 flex-col py-10 lg:py-20">
-        <slot/>
+        <slot />
       </main>
-
-      <FooterPartial/>
-        
     </div>
 
     <ClientOnly>
-    <ResponsiveNav/>
+      <ResponsiveNav /> <!-- TODO figure out wtf this is -->
     </ClientOnly>
-    
   </div>
 </template>
 
