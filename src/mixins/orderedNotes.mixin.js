1e8e07673b08b1e58aef2e0afe5fd04e59f33447
Adam <adam.m.leis@gmail.com>
2020-07-20 16:13:42 -0400

    make default computed order in mixin

--- src/mixins/orderedNotes.mixin.js
+++ src/mixins/orderedNotes.mixin.js
@@ -4,7 +4,7 @@ import noteOrders from '~/order';
 export default {
   // vm.category and vm.allNotes should be set on whatever component uses this mixin
   computed: {
-    order: vm => vm.category ? noteOrders[vm.category] : [],
+    order: vm => (vm.category ? noteOrders[vm.category] : []) || [],
     orderedNotes: vm => vm.allNotes.reduce((notes, { node: note }) => {
       // this note the right category? no? ignore it
       if (note.category && note.category.title === vm.category) {
